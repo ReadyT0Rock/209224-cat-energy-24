69f8fe6fa928c1cfc90fe33b48993764280a9513
ReadyT0Rock <Egor-Miass@yandex.ru>
2022-02-02 02:56:51 +0500

    Добавленое открытое меню при не работающем js, правлена сетка в форме в пункте контактные данные на декстопе

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,15 @@
 var navMain = document.querySelector(".navigation");
-var navToggle = document.querySelector(".page-header__button--open");
+var navToggle = document.querySelector(".page-header__button--close");
 
   navMain.classList.remove("navigation--nojs");
   navToggle.addEventListener("click", function() {
     if (navMain.classList.contains("navigation--closed")) {
       navMain.classList.remove("navigation--closed");
       navMain.classList.add("navigation--opened");
+      navToggle.classList.add("page-header__button--opened");
     } else {
       navMain.classList.add("navigation--closed");
       navMain.classList.remove("navigation--opened");
+      navToggle.classList.remove("page-header__button--opened");
     }
 });
